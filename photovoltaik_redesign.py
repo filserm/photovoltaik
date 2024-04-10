05a4ac3b1f04b67f19f62062272738ddf502c5a3
Michael Filser <filser.michael@gmail.com>
2024-04-10 21:36:14 +0200

    changes

--- photovoltaik_redesign.py
+++ photovoltaik_redesign.py
@@ -89,7 +89,7 @@ halle = PVAnlage("halle")
 
 anlagen = []
 anlagen.append(mike)
-anlagen.append(halle)
+#anlagen.append(halle)
 
 MAX_DAYS = 10000
 
@@ -212,6 +212,7 @@ def make_graph(path_db="", path_png="", year="", plot_filename="", colors="", wa
     max_value_this_year_dict[name[0]] = [max_val_day, max_val_val]
 
     df_last7days = df.head(7)
+    df_last7days = df_last7days.copy()
     df_last7days['Datum'] = pd.to_datetime(df_last7days['Datum'], dayfirst=True)
     df_last7days.sort_values(by=['Datum'], inplace=True)
     kum_value_7days = df_last7days['HausGesamt'].sum().astype(int) 
@@ -267,6 +268,7 @@ def make_graph(path_db="", path_png="", year="", plot_filename="", colors="", wa
     text_color_wr = 'silver'
 
     df_wr = df.head(7)
+    df_wr = df_wr.copy()
     #df_wr.drop(df_wr.head(1).index,inplace=True)
     df_wr['Datum'] = pd.to_datetime(df_wr['Datum'], dayfirst=True)
     df_wr.sort_values(by=['Datum'], inplace=True)
@@ -290,7 +292,7 @@ def make_graph(path_db="", path_png="", year="", plot_filename="", colors="", wa
 
     ## multiple bar chart 
     X_axis = np.arange(len(df_wr['Datum']))
-    if df_wr.iloc[0][8] == -1:  # dann WR3 nicht vorhanden
+    if df_wr.iloc[0, 8] == -1:  # dann WR3 nicht vorhanden
         ax3.bar(X_axis-0.2, df_wr['WR1'],width=0.4, color=color_wr[0], label='kWh')
         ax3.bar(X_axis+0.2, df_wr['WR2'],width=0.4, color=color_wr[1], label='kWh') 
     else:
@@ -325,7 +327,7 @@ def make_graph(path_db="", path_png="", year="", plot_filename="", colors="", wa
     #### END END END Wechselrichter END END END #####
 
     df['HausGesamt'] = df['HausGesamt'].astype(float)  
-    df['Datum']      = pd.to_datetime(df['Datum'])
+    df['Datum']      = pd.to_datetime(df['Datum'], format='mixed')
 
     #print (df.tail(30))
     #df.index = pd.DatetimeIndex(df.Datum)
@@ -340,7 +342,7 @@ def make_graph(path_db="", path_png="", year="", plot_filename="", colors="", wa
     df_avg = df.loc[df.groupby("Monatabs")["haus_sum_monatabs"].idxmax()]
     df_avg1 = df_avg[df_avg['haus_sum_monatabs'] != 0]
     df_avg2 = df_avg1[df_avg1['Jahr'] != int(current_year)]
-    df_mean = df_avg2.groupby("Monat").agg({"haus_sum_monatabs" : np.mean}).reset_index()
+    df_mean = df_avg2.groupby("Monat").agg({"haus_sum_monatabs" : "mean"}).reset_index()
     
     df_max = df.loc[df.groupby("Monat")["haus_sum_monatabs"].idxmax()]
     df_max['haus_max_monat']     = df_avg['haus_sum_monatabs']
